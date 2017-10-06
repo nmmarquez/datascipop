2b2f6a9fca1cf2b933d71f4019386c8526f7b4a3
Neal Marquez <nmmarquez@gmail.com>
2017-10-06 08:45:58 -0700

    update hw with comments

--- LabWeek1_baby_names/hw1.R
+++ LabWeek1_baby_names/hw1.R
@@ -11,67 +11,102 @@ data <- do.call(rbind, lapply(c(state.abb, "DC"), function(x)
 names(data) <- c("State", "Sex", "Birthyear", "Name", "Frequency")
 
 # Question 1
+## Name and state to use in analysis
 testname <- "Alex"
 teststate <- "CO"
 
 testdatasubset <- data %>% 
+    # filter data by name and state
     filter(Name == testname & State == teststate) %>%
-    group_by(Birthyear) %>% 
+    # group by the year
+    group_by(Birthyear) %>%
+    # get the sum of the counts for each year 
     summarize(Namecount=sum(Frequency))
 
 statedatasubset <- data %>% 
+    # filter by state
     filter(State == teststate) %>%
+    # group by year
     group_by(Birthyear) %>% 
+    # get the count of all names for each year
     summarize(Birthcount=sum(Frequency))
 
-namepdata <- left_join(statedatasubset, testdatasubset, by="Birthyear") %>% 
+namepdata <- statedatasubset %>%
+    # join the two data sets together using the all name data as full key
+    left_join(testdatasubset, by="Birthyear") %>% 
+    # Calculate the probability of test name
     mutate(Namecount = replace(Namecount, is.na(Namecount), 0), 
            Namep=Namecount / Birthcount)
 
+# plot the time series
 ggplot(namepdata, aes(x=Birthyear, Namep)) + geom_point()
 
 # Question 2
 
 sexratiodata <- data %>% 
+    # filter by name and state
     filter(Name == testname & State == teststate) %>%
-    group_by(Birthyear, Sex) %>% 
+    # group by year and sex
+    group_by(Birthyear, Sex) %>%
+    # get the count of each year for both sexs
     summarize(Namecount=sum(Frequency)) %>%
-    right_join(unique(data[,c("Sex", "Birthyear")])) %>% 
-    group_by(Birthyear) %>% 
+    # join on a dataset that has all year sex combinations
+    right_join(unique(data[,c("Sex", "Birthyear")])) %>%
+    # group by the birth year
+    group_by(Birthyear) %>%
+    # calculate the ratio
     summarize(Ratio = Namecount[Sex=="F"]/Namecount[Sex=="M"])
 
+# plot the time series
 ggplot(data=sexratiodata, aes(x=Birthyear, y=Ratio)) + geom_point()
 
 # Question 3 
 
+# read in the life table data set filtering only for 2015
 (LT <- read.table("./bltper_1x1.txt", skip=2, header=T) %>%
         mutate(Age=as.numeric(as.character(Age))) %>% 
         filter(Year+Age==2015))
 
+# calculate the probability of being alive for all ages in 2015
 (palive <- LT %>% 
     mutate(Fsurvive=lx/100000) %>%
     select(Year, Age, Fsurvive))
 
-(namesubset <- data %>% 
+(namesubset <- data %>%
+    # subset by name
     filter(Name == testname) %>%
-    group_by(Birthyear) %>% 
+    # group by birth year
+    group_by(Birthyear) %>%
+    # get the count of the names 
     summarize(Namecount=sum(Frequency)) %>%
     mutate(Year=Birthyear) %>%
     left_join(palive, by="Year") %>%
     mutate(Alive=Namecount*Fsurvive))
 
 (namesubsetstate <- data %>% 
+        # subset by name
         filter(Name == testname) %>%
-        group_by(Birthyear, State) %>% 
+        # group by year and state
+        group_by(Birthyear, State) %>%
+        # get the sum of the name count 
         summarize(Namecount=sum(Frequency)) %>%
+        # Cretae new year variable so that the names match
         mutate(Year=Birthyear) %>%
+        # join datatsets on years
         left_join(palive, by="Year") %>%
+        # estimate number of individuals alive based on survival prob
         mutate(Alive=Namecount*Fsurvive))
 
-(meanageallstates <- namesubsetstate %>% mutate(Alivew=Alive*Age) %>%
-        group_by(State) %>% 
+(meanageallstates <- namesubsetstate %>% 
+        # Create an age weighted alive number variable
+        mutate(Alivew=Alive*Age) %>%
+        # group by state across all years
+        group_by(State) %>%
+        # calcualte aggregated alive and alive weighted numbers
         summarise(personyears=sum(Alive, na.rm=T), 
                   personyearsw=sum(Alivew, na.rm=T)) %>%
+        # get the mean year of birth by subtracting age from 2015
         mutate(mean_year=2015-personyearsw/personyears))
 
+# histogram of data
 qplot(meanageallstates$mean_year)
